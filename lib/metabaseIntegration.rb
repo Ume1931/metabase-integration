565475c1b8356951b2559eb1ed238f9415ce9a90
Nate Wilson <nwilson@blackanchor.io>
2018-07-06 10:37:38 +0500

    Permitted params removed

--- lib/metabaseIntegration.rb
+++ lib/metabaseIntegration.rb
@@ -1,10 +1,11 @@
 require "metabaseIntegration/version"
+require 'jwt'
 
 module MetabaseIntegration
-  def self.payload(dashboard_id,params)
+  def self.payload(dashboard_id)
     {
         :resource => {:dashboard => dashboard_id},
-        :params => params
+        # :params => params
     }
   end
 
@@ -23,7 +24,7 @@ module MetabaseIntegration
 
   def self.jwt_endode_metabase(dashboard_id)
     secret_keys =  Rails.application.secrets
-    parameters = permitted_params.empty? ? {} : permitted_params
-    return JWT.encode payload(dashboard_id,parameters), secret_keys.metabase_secret_key
+    # parameters = permitted_params.empty? ? {} : permitted_params
+    return JWT.encode payload(dashboard_id), secret_keys.metabase_secret_key
   end
 end
